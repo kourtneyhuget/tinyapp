858f49abb264080eee6723e3172079fe57c6ed4b
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-09-28 20:28:38 +0000

    added second route and template

--- express_server.js
+++ express_server.js
@@ -9,6 +9,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
